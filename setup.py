1830da583b7df2cecbe99a613e72604a392cfa40
Jan Range <30547301+JR-1991@users.noreply.github.com>
2022-03-22 13:46:25 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="pyPUMA",
+    version="0.0.1",
+    author="Range, Jan",
+    author_email="jan.range@simtech.uni-stuttgart.de",
+    license="BSD2 Clause",
+    packages=setuptools.find_packages(),
+    install_requires=[
+      "pydantic,
+      "dictdiffer",
+      "pandas",
+    ]
+)
